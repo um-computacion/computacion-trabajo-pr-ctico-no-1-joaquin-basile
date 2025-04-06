aa86d6a415c61551dab377b4548f21b3a8694dc4
joaquin-basile <j.basile@alumno.um.edu.ar>
2025-04-06 19:21:37 -0300

    Se creo la estructura del proyecto junto con los test necesarios para la app

---
+++ tests/test_roman_converter.py
@@ -0,0 +1,30 @@
+import unittest
+from src.roman_converter import decimal_to_roman
+
+class TestRomanConverter(unittest.TestCase):
+    def test_basic_numbers(self):
+        self.assertEqual(decimal_to_roman(1), "I")
+        self.assertEqual(decimal_to_roman(5), "V")
+        self.assertEqual(decimal_to_roman(10), "X")
+
+    def test_subtraction_rules(self):
+        self.assertEqual(decimal_to_roman(4), "IV")
+        self.assertEqual(decimal_to_roman(9), "IX")
+        self.assertEqual(decimal_to_roman(40), "XL")
+        self.assertEqual(decimal_to_roman(90), "XC")
+
+    def test_complex_numbers(self):
+        self.assertEqual(decimal_to_roman(49), "XLIX")
+        self.assertEqual(decimal_to_roman(99), "XCIX")
+        self.assertEqual(decimal_to_roman(499), "CDXCIX")
+        self.assertEqual(decimal_to_roman(999), "CMXCIX")
+        self.assertEqual(decimal_to_roman(3999), "MMMCMXCIX")
+
+    def test_additions_rules(self):
+        self.assertEqual(decimal_to_roman(8), "VIII")
+        self.assertEqual(decimal_to_roman(20), "XX")
+        self.assertEqual(decimal_to_roman(300), "CCC")
+
+if __name__ == '__main__':
+    unittest.main()
+
